c57b5262b092aa818522c611d5fe7c5d75a2c1c7
diegomezcota <gomezcotadiego@gmail.com>
2020-10-30 12:37:28 -0700

    FactSales data insertion

--- Dimensions/FactSales/valuesUpdate.sql
+++ Dimensions/FactSales/valuesUpdate.sql
@@ -1,11 +1,17 @@
 insert into FactSales
     select 
-        od.productID,
-        o.employeeID,
-        o.customerID,
-        o.OrderData,
+        od.ProductID,
+        o.EmployeeID,
+        cast(o.CustomerID as char(5)),
+        o.OrderDate,
         od.OrderID,
         od.Quantity,
-        od.unitPrice,
+        od.UnitPrice,
         od.Discount,
+        cast(od.Quantity*od.unitPrice*od.Discount as money),
+        cast(od.Quantity*od.unitPrice*(1-od.Discount) as money)
+    from 
+        NorthwindDB.dbo.[Order Details] od, NorthwindDB.dbo.Orders o
+    where
+        od.OrderID = o.OrderID
         
