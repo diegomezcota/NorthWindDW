fa8b8ffaf52b38996bf1412ecd1a215d9e1a99a1
Domene99 <jadomene99@gmail.com>
2020-11-03 16:05:48 -0600

    Add missing sql queries
    Missing dwh queries

--- queries.sql
+++ queries.sql
@@ -40,3 +40,29 @@ JOIN Products AS P ON OD.ProductID = P.ProductID
 JOIN Orders AS O ON OD.OrderID = O.OrderID
 WHERE YEAR(O.OrderDate) = 1997
 ORDER BY SUM((OD.UnitPrice * OD.Quantity) * (1 - OD.Discount)) DESC;
+
+-- Region que generó más ventas en 1997
+CREATE FUNCTION region_ventas_max()
+	RETURNS
+	SELECT TOP 1 O.ShipRegion
+	FROM Orders AS O
+	JOIN OrderDetails AS OD ON OD.OrderId = O.OrderId
+	WHERE YEAR(O.OrderDate) = 1997
+	ORDER BY SUM(OD.UnitPrice * OD.Quantity) DESC;
+
+-- Estado o pais que mas genero de la region de ventas maxima
+SELECT IF(
+	STRCMP(region_ventas_max(), "USA") = 0,
+	O.ShipState,
+	O.ShipCountry)
+FROM Orders as O
+JOIN OrderDetails AS OD ON OD.OrderId = O.OrderId
+WHERE YEAR(O.OrderDate) = 1997
+ORDER BY SUM(OD.UnitPrice * OD.Quantity) DESC;
+
+-- Total de ventas org por region, estado y pais
+SELECT SUM(OD.UnitPrice * OD.Quantity) AS Ventas,
+O.ShipRegion, O.ShipState, O.ShipCountry
+FROM OrderDetails AS OD
+JOIN Orders AS O ON O.OrderId = OD.OrderId
+GROUP BY O.ShipRegion, O.ShipState, O.ShipCountry
